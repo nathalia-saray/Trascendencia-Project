41cb7e0b7063853689b325aa0f75d4cda23a18a7
Nathalia Saray <nathalia.saray@ibm.com>
2021-12-11 03:18:51 -0500

    update db files

--- config/db.php
+++ config/db.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+define('DB_NAME', 'db_trascendencia');//Nombre de la base de datos
 ?>
